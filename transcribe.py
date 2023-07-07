7fa1b3db075425ec3c1db2d2140b329f3c248884
MALINENI SRIKANTH <87704093+Srikanth635@users.noreply.github.com>
2023-07-07 15:29:16 +0200

    Update transcribe.py

--- transcribe.py
+++ transcribe.py
@@ -5,27 +5,39 @@ import time
 # nltk.download('punkt')
 # from nltk.tokenize import sent_tokenize
 from datetime import datetime,timedelta
+import rasa
 
-text = """they are right, right, right, from prepping with our 10 points. Right, necessary. Okay. Right, just hand.
-Yeah. Yeah, what I need from you is if you can, even before the hands, can you send, I shift the constraints based map, or can you create a pull request?"""
-
-# print(sent_tokenize(text))
-
-dt = datetime.now()
-print(dt)
-dt1 = dt+timedelta(seconds=20)
-print("UNIX : ",time.mktime(dt.timetuple()))
-print("UNIX : ",time.mktime(dt1.timetuple()))
-# # torch.cuda.is_available()
-# DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
-# print(DEVICE)
+# import spacy
+# sp = spacy.load('en_core_web_lg')
+# sen = sp(u"Move the cup from the table")
+# verbs = []
+# for word in sen:
+#     print(f'{word.text:{12}} {word.pos_:{10}} {word.tag_:{8}} {spacy.explain(word.tag_)}')
+#     if word.pos_ == "VERB":
+#         verbs.append((word.text,word.pos_))
 #
-# wav = '16_17_55.wav'
-# # Whisper Params
-# model = whisper.load_model('base',device=DEVICE)
-# result = model.transcribe(wav)
-# print(result.keys())
-# print(result["text"])
-# print(result["segments"][0])
-# print(result["segments"])
-# print(result["language"])
+# print("VERBS: ",verbs)
+#
+# for entity in sen.ents:
+#     print("FIRST ENTS : ",entity.text + ' - ' + entity.label_ + ' - ' + str(spacy.explain(entity.label_)))
+#
+# from spacy.tokens import Span
+#
+# ORG = sen.vocab.strings[u'PRODUCT']
+# new_entity = Span(sen, 2, 3, label=ORG)
+# sen.ents = list(sen.ents) + [new_entity]
+#
+# for entity in sen.ents:
+#     print("Second ENTS : ",entity.text + ' - ' + entity.label_ + ' - ' + str(spacy.explain(entity.label_)))
+
+
+#####################################################     RASA      ####################################################
+# rasa run --enable-api --debug
+
+import requests
+# text = requests.form.get('query')
+payload = {"sender": "Rasa", "text": "pour water"}
+headers = {'content-type': 'application/json'}
+response = requests.post('http://localhost:5005/model/parse', json=payload, headers=headers)
+result = response.json()
+print(result)
